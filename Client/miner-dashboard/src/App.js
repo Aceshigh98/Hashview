d847e605e669397b1001e4af4eda4e72e891a1f8
AcesHigh <cothermanbrannon@gmail.com>
2024-02-19 15:48:16 -0600

    *Setting up project* first commit 02/19/2024

---
+++ Client/miner-dashboard/src/App.js
@@ -0,0 +1,13 @@
+//import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+import Login from './Pages/login';
+
+import './App.css';
+
+function App() {
+  return (
+    <div></div>
+  );
+}
+
+export default App;
