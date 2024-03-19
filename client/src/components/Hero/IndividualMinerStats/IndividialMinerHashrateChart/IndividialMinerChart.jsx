7b4e427e0315d3b25123b6348f5fd2017444f19b
AcesHigh <cothermanbrannon@gmail.com>
2024-03-18 20:18:59 -0500

    3/18/2023

--- client/src/components/Hero/IndividualMinerStats/IndividialMinerHashrateChart/IndividialMinerChart.jsx
+++ client/src/components/Hero/IndividualMinerStats/IndividialMinerHashrateChart/IndividialMinerChart.jsx
@@ -1,10 +1,65 @@
 import React from 'react';
 import classes from './IndividualMinerChart.module.css'
+import ReactApexCharts from 'react-apexcharts';
 
 const IndividialMinerChart = () => {
+
+  //Define options for the chart
+
+  var options = {
+    chart: {
+      height: 280,
+      type: "area",
+      dropShadow: {
+        enabled: true,
+        top: 0,
+        left: 0,
+        blur: 3,
+        opacity: 0.5
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    series: [
+      {
+        name: "Series 1",
+        data: [45, 52, 38, 45, 19, 23, 2],
+        color: '#f0ba33' 
+      }
+    ],
+    fill: {
+      type: "gradient",
+      gradient: {
+        shadeIntensity: 1,
+        opacityFrom: 1,
+        opacityTo: 0.8,
+        stops: [0, 90, 100]
+      }
+    }
+    ,
+    xaxis: {
+      categories: [
+        "01 Jan",
+        "02 Jan",
+        "03 Jan",
+        "04 Jan",
+        "05 Jan",
+        "06 Jan",
+        "07 Jan"
+      ]
+    }
+  };
   return (
-    <div className={classes['container']}>IndividialMinerChart</div>
+    <div className={classes['container']}>
+        <ReactApexCharts
+         options={options}
+         series={options.series}
+         type="area"
+         width="100%"
+         />
+    </div>
   )
 }
 
-export default IndividialMinerChart
+export default IndividialMinerChart;
--- client/src/components/Hero/IndividualMinerStats/IndividualMinerRevenueChart/IndividualMinerRevenueChart.jsx
+++ client/src/components/Hero/IndividualMinerStats/IndividualMinerRevenueChart/IndividualMinerRevenueChart.jsx
@@ -1,10 +1,66 @@
 import React from 'react';
 import classes from './IndividualMinerRevenueChart.module.css';
+import ReactApexCharts from 'react-apexcharts';
 
 const IndividualMinerRevenueChart = () => {
-  return (
-    <div className={classes['container']}>IndividualMinerRevenueChart</div>
-  )
+  
+    //Define options for the chart
+
+    var options = {
+      chart: {
+        height: 100,
+        type: "area",
+        dropShadow: {
+          enabled: true,
+          top: 0,
+          left: 0,
+          blur: 3,
+          opacity: 0.8
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      series: [
+        {
+          name: "Series 1",
+          data: [45, 52, 38, 45, 19, 23, 2],
+          color: '#f0ba33' 
+        }
+      ],
+      fill: {
+        type: "gradient",
+        gradient: {
+          shadeIntensity: 1,
+          opacityFrom: 1,
+          opacityTo: 0.8,
+          stops: [0, 90, 100]
+        }
+      }
+      ,
+      xaxis: {
+        categories: [
+          "01 Jan",
+          "02 Jan",
+          "03 Jan",
+          "04 Jan",
+          "05 Jan",
+          "06 Jan",
+          "07 Jan"
+        ]
+      }
+    };
+    return (
+      <div className={classes['container']}>
+          <ReactApexCharts
+           options={options}
+           series={options.series}
+           type="area"
+           width="100%"
+           height="auto"
+           />
+      </div>
+    )
 }
 
-export default IndividualMinerRevenueChart
+export default IndividualMinerRevenueChart;
