02d0f28df40e433657355c8acfe57aaa87c543b7
AcesHigh <cothermanbrannon@gmail.com>
2024-06-04 14:36:28 -0500

    6/4/2024

--- client/src/components/MainMinerStats/MainMinerStats.jsx
+++ client/src/components/MainMinerStats/MainMinerStats.jsx
@@ -24,7 +24,7 @@ const MainMinerStats = () => {
       try {
         // Fetch miner details
         const minerDetailsResponse = await axios.post(
-          "http://localhost:5000/api/data/minersDetails",
+          "http://api.hash-view.com:5000/api/data/minersDetails",
           {
             userName: user.userName,
           },
--- client/src/hooks/useLogin.js
+++ client/src/hooks/useLogin.js
@@ -13,7 +13,7 @@ export const useLogin = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/user/login",
+        "http://api.hash-view.com:5000/api/user/login",
         {
           userName,
           password,
