c63dd76183229822dce1dd3b5c713a13685f582c
AcesHigh <cothermanbrannon@gmail.com>
2024-06-07 15:12:18 -0500

    6/7/2024

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 const cors = require("cors");
 const express = require("express");
-const https = require("https");
-const fs = require("fs");
 const minersRoutes = require("./routes/minersRoute");
 const userRoutes = require("./routes/user");
 const tasks = require("./tasks/cronTasks");
+const http = require("http");
 
 // Connecting to MongoDB
 const connectDB = require("./database/dbConfig");
 
-const PORT = process.env.PORT || 443; // Change to the HTTPS port
+// Ensure the environment variable PORT is defined
+const PORT = process.env.PORT || 5000;
 
 // Scheduled tasks
 tasks();
@@ -17,28 +17,6 @@ tasks();
 connectDB(); // Establish the database connection
 
 const app = express();
-
-// Let's Encrypt SSL Certificate
-const privateKey = fs.readFileSync(
-  "/etc/letsencrypt/live/www.hash-view.com/privkey.pem",
-  "utf8"
-);
-const certificate = fs.readFileSync(
-  "/etc/letsencrypt/live/www.hash-view.com/cert.pem",
-  "utf8"
-);
-const ca = fs.readFileSync(
-  "/etc/letsencrypt/live/www.hash-view.com/chain.pem",
-  "utf8"
-);
-
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca,
-};
-
-const httpsServer = https.createServer(credentials, app);
 
 app.use(express.json()); // For parsing application/json
 app.use(cors());
@@ -48,7 +26,9 @@ app.use("/api/user", userRoutes);
 // Protected Routes ... Miners Data and User Data
 app.use("/api/data", minersRoutes);
 
-// Start HTTPS Server
-httpsServer.listen(PORT, () => {
-  console.log("Server is running on port 5000 over HTTPS.");
+// Create HTTP server
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
